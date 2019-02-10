db6ff25d03f0c97648477373895b9a920e85aec1
jose6rojas <jose99rojas@hotmail.com>
2019-02-09 23:19:14 -0600

    Updates

---
+++ precache-manifest.e6801aceee5f6433ba07926cf941d0d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a4430c086131fba29bb",
+    "url": "/TicTacToe/static/js/runtime~main.1a4430c0.js"
+  },
+  {
+    "revision": "777b2b43faf7059b4bfd",
+    "url": "/TicTacToe/static/js/main.777b2b43.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/TicTacToe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "777b2b43faf7059b4bfd",
+    "url": "/TicTacToe/static/css/main.c6adf368.chunk.css"
+  },
+  {
+    "revision": "89888cc957a21804fa13b49322b3ed59",
+    "url": "/TicTacToe/index.html"
+  }
+];
